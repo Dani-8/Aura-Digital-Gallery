033c89d854ff55a69e6d927b340795e7c35d58b8
Dani-8 <daniyalw6190@gmail.com>
2026-05-21 00:54:38 +0500

    Updates

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from "express"
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("HI")
+})
+
+app.listen("4000", ()=>{
+    console.log("Server is running on http://localhost:4000/");
+    
+})
