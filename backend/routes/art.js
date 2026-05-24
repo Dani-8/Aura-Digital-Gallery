8d7a0259dd91d2f2a38efb7733e51579b84b3d30
Dani-8 <daniyalw6190@gmail.com>
2026-05-24 22:51:48 +0500

    Updates

--- backend/routes/art.js
+++ backend/routes/art.js
@@ -13,3 +13,26 @@ router.get("/", async (req, res) => {
         res.status(500).json({ message: err.message})
     }
 })
+
+
+// POST new art
+router.post("/", async (req, res) => {
+    try {
+        const newArt = new Art(req.body)
+        const savedArt = await newArt.save()
+        res.status(201).json(savedArt)
+    } catch(err){
+        res.status(500).json({ message: err.message})
+    }
+})
+
+
+// DELETE art by id
+router.delete("/:id", async (req, res) => {
+    try{
+        await Art.findByIdAndDelete(req.params.id)
+        res.json({ message: "Artwork deleted successfully"})
+    } catch(err){
+        res.status(500).json({ message: err.message})
+    }
+})
