8abaa88306ec911e4b7b666eb9ad5aec9b121640
Dani-8 <daniyalw6190@gmail.com>
2026-05-24 22:43:39 +0500

    Updates

--- backend/models/Art.js
+++ backend/models/Art.js
@@ -1,3 +1,12 @@
 import mongoose from "mongoose"
 
+const ArtSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    artist: { type: String, required: true },
+    url: { type: String, required: true },
+    medium: String,
+    price: String,
+    createdAt: { type: Date, default: Date.now }
+})
 
+export default mongoose.model("Art", ArtSchema)
