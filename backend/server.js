8e80219e8cdad907ae85c1890458a8dff8e7b844
Dani-8 <daniyalw6190@gmail.com>
2026-05-24 22:58:00 +0500

    Updates

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,25 @@
 import express from "express"
 import cors from "cors"
 
+import mongoose from "mongoose"
+import yaml from "js-yaml"
+import fs from "fs"
+
+import dotenv from "dotenv"
+dotenv.config()
+
+let config = {}
+try {
+    config = yaml.load(fs.readFileSync("./config.yaml", "utf8"))
+} catch (err) {
+    console.error("Error loading config file:", err)
+}
+// =====================================================================
+// =====================================================================
+// =====================================================================
+
+
+
 const app = express()
 
 app.get("/", (req, res) => {
